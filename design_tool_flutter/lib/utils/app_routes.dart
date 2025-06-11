b791042f2bc802619c20bb512b821582119a9a1f
Atwin0401 <aayushshkl@outlook.com>
2025-06-11 23:50:42 +0530

    Add design_tool_flutter/lib/utils/app_routes.dart 1749666042329

---
+++ design_tool_flutter/lib/utils/app_routes.dart
@@ -0,0 +1,11 @@
+class AppRoutes {
+  static const splash = '/';
+  static const login = '/login';
+  static const home = '/home';
+  static const project = '/projects';
+  static const editor = '/editor';
+  static const profile = '/profile';
+  static const settings = '/settings';
+  static const collaboration = '/collaboration';
+  static const prototype = '/prototype';
+}
